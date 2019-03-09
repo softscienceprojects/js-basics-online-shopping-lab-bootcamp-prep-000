f565a26b7d9d5b2ec5947bdb79b4814e35970bdb
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 16:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,14 +47,26 @@ function total() {
 };
 
 
+// function removeFromCart(item) {
+//   var myItem = item;
+//   for (let i=0; i<cart.length; i++) {
+//     if (cart[i].hasOwnProperty(item) === true) {
+//       cart.splice(i,1)
+//     } return cart
+//   } return "That item is not in your cart."
+// };
+
 function removeFromCart(item) {
-  var myItem = item;
-  for (let i=0; i<cart.length; i++) {
-    if (cart[i].hasOwnProperty(item) === true) {
-      cart.splice(i,1)
-    } return cart
-  } return "That item is not in your cart."
-};
+  var present = false
+  if (cart.length > 0){
+  for (var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item) === true){
+      cart.splice(i, 1)
+      console.log(cart)
+      present = true}}}
+  if (present === false){console.log('That item is not in your cart.')}
+  return cart
+}
 
 function placeOrder(cardNumber) {
   for (var i=0; i<cart.length; i++) {
