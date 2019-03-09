78a6cdba242f391e7c7c4043862c387a5bb79109
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 16:52:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ function removeFromCart(item) {
   for (let i=0; i<cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
-      return cart;
-    }
+      
+    } return cart;
   } return "That item is not in your cart."
 };
 
