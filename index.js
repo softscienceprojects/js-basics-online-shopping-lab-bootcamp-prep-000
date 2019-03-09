6ac4859fc9a8adb04587c35dd70f87141a895f71
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 17:06:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,9 +51,8 @@ function removeFromCart(item) {
   for (var i=0; i<cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
-
-    } return cart;
-  } return "That item is not in your cart."
+    } 
+  } return cart;
 };
 
 function placeOrder(cardNumber) {
