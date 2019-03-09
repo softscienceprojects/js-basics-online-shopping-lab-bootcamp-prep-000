650e23d3b3ff0ef696046114bfc25022018c7eac
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 16:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function total() {
 //       if (property1 === item) {
 //         cart.splice(i, 1);
 //       } return cart
-//     } 
+//     }
 //   } return "That item is not in your cart."
 // };
 
