0f39d7e5fe3561ba3844e9522ab857faaea1b42e
erin johnson <erin.nicole.johnson@gmail.com>
2019-03-09 16:05:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i=0; i<cart.length; i++) {
-    if (cart[i]['itemName'] === item ){
+    if (cart[i]['itemName'] == item ){
       var removed = cart.splice(cart.length-1, 1);
     } return removed
   } return "That item is not in your cart."
